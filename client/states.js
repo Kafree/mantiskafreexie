243540f2a500ffe69c696b69b2164a6bd51af45c
Kafree Xie <kafree.xie@flex.com>
2017-10-22 23:35:46 -0700

    [Studio] Add screen "FormLarge-Classic"

--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.formlargeclassic', {
+        views: {
+            app: {
+                controller: 'app_formlargeclassic',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.formlargeclassic');
+                }
+            }
+        }
     });
 });
