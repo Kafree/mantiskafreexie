faaac716c6484b4fb7797fab56c40a1c003030a6
Kafree Xie <kafree.xie@flex.com>
2017-10-24 03:54:50 -0700

    [Studio] Add screen "Login-Classic"

--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.loginclassic', {
+        views: {
+            app: {
+                controller: 'app_loginclassic',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.loginclassic');
+                }
+            }
+        }
     });
 });
